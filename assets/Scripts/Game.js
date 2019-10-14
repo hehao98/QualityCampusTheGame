6726ac9ead542cdd4cf0ba958c9fdc1383fcc7be
Hao He <hehao1998@pku.edu.cn>
2019-10-14 18:22:03 +0800

    Added Skeleton Resource System

--- assets/Scripts/Game.js
+++ assets/Scripts/Game.js
@@ -8,6 +8,9 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
+const TICKS_SEMESTER = 20 * 7 * 5;
+const TICKS_WEEK = 7 * 5;
+
 cc.Class({
     extends: cc.Component,
 
@@ -18,26 +21,25 @@ cc.Class({
         timeSinceLastUpdate: 0,
         isPaused: false,
         timeString: "",
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+
+        // Properties for resource management
+        fund: 0,
+        fundModifiers: {
+            default: [],
+            type: [cc.Object]
+        },
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        let that = this;
+        cc.loader.loadRes('InitialData', function (err, jsonAsset) {
+            jsonAsset.json.fundModifiers.forEach((modifier) => {
+                that.addFundModifier(modifier);
+            });
+        });
+    },
 
     start () {
         this.timeString = this.getTickString();
@@ -51,6 +53,9 @@ cc.Class({
                 this.currentTick++;
                 this.timeSinceLastUpdate -= this.speedModifier;
                 this.timeString = this.getTickString();
+
+                // Update corresponding game logic
+                this.updateResource(this.currentTick);
             }
         }
     },
@@ -76,5 +81,37 @@ cc.Class({
         let weekDay = Math.floor((this.currentTick % (7 * 5)) / 5);
         let day = dayTimeStr[this.currentTick % 5];
         return semester + "学期第" + week + "周星期" + weekStr[weekDay] + " " + day;
+    },
+
+    updateResource(tick) {
+        let that = this;
+        let toBeRemoved = [];
+        this.fundModifiers.forEach(modifier => {
+            if (modifier.type === "once") {
+                that.fund += modifier.amount;
+                toBeRemoved.push(modifier.name);
+            } else if (modifier.type === "interval") {
+                if (modifier.interval === "semester" && tick % TICKS_SEMESTER === 0) {
+                    that.fund += modifier.amount;
+                }
+                if (modifier.interval === "week" && tick % TICKS_WEEK === 0) {
+                    that.fund += modifier.amount;
+                }
+            }
+        });
+        toBeRemoved.forEach(name => { this.removeFundModifier(name) });
+    },
+
+    addFundModifier(modifier) {
+        // Apply sanity checks
+        console.assert(modifier.hasOwnProperty("name"));
+        console.assert(modifier.hasOwnProperty("type"));
+        console.assert(modifier.hasOwnProperty("amount"));
+
+        this.fundModifiers.push(modifier);
+    },
+
+    removeFundModifier(modifierName) {
+        this.fundModifiers = this.fundModifiers.filter(modifier => (modifier.name !== modifierName));
     }
 });
