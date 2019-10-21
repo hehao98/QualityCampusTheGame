4d8b34f40de8872592bf78b638742b9ec320197f
Hao He <hehao1998@pku.edu.cn>
2019-10-21 17:12:26 +0800

    Added A Loading Screen

--- assets/Scripts/UI/MainMenu.js
+++ assets/Scripts/UI/MainMenu.js
@@ -3,11 +3,26 @@
 cc.Class({
     extends: cc.Component,
 
-    properties: {},
+    properties: {
+        loadingWindow: cc.Node,
+        loadingLabel: cc.Label,
+    },
 
     start () {},
 
     startGame() {
-        cc.director.loadScene("Game");
+        cc.director.preloadScene("Game", () => {
+            cc.director.loadScene("Game");
+        });
+        this.loadingWindow.active = true;
+        let label = this.loadingLabel;
+        let cnt = 1;
+        label.schedule(() => {
+            label.string = "加载中";
+            for (let i = 0; i < cnt % 4; ++i) {
+                label.string += ".";
+            }
+            cnt += 1;
+        }, 0.5);
     }
 });
