eff7b13fd30ffebb234cf350118afd5aa54719bb
TP <173043733@qq.com>
2019-10-27 18:28:10 +0800

    refactor: <classname>.prototype style methods for BuildingManager.

--- assets/Scripts/GameLogic/BuildingManager.js
+++ assets/Scripts/GameLogic/BuildingManager.js
@@ -12,69 +12,72 @@ function BuildingManager(properties) {
     this.nextBuildingComponentID = 0;
     this.buildings = [];
 
-    // methods
-    /**
-     * 
-     * @param {Object} properties.type - The type of the building 
-     */
-    this.add = function (properties) {
-        let r = _.cloneDeep(properties);
-        r["id"] = this.nextBuildingID++;
-        this.buildings.push(createBuilding(r));
-
-    };
-
-    /**
-     * 
-     * @param {Object} properties.id - ID of the building 
-     */
-    this.upgrade = function (properties) {
-        let target = _.find(
-            this.buildings,
-            function (building) { return building.id === properties.id; }
-        );
-        if (target === undefined) {
-            throw new ReferenceError("Building ID not exists.");
-        }
-        else {
-            target.upgrade();
-        }
-    };
-
-    /**
-     * 
-     * @param {Object} properties.buildingID - ID of the building
-     * 
-     */
-    this.addComponent = function (properties) {
-        let target = _.find(
-            this.buildings,
-            function (building) {
-                return building.id === properties.buildingID;
-            }
-        );
-        if (target === undefined) {
-            throw new ReferenceError("Building ID not exists.");
-        }
-        else {
-            let properties_revised = _.cloneDeep(properties);
-            properties_revised["id"] = this.nextBuildingComponentID++;
-            target.addComponent(properties_revised);
-        }
-    };
-
-    this.debugPrint = function () {
-        console.log("[BuildingManager DebugPrint]");
-        console.log("building number: " + this.buildings.length);
-        for (let b of this.buildings) {
-            b.debugPrint();
-        }
-        console.log("------------------------------------------------------");
-    };
 
     // constructor left-overs
 
 }
+
+// methods
+/**
+ * 
+ * @param {Object} properties.type - The type of the building 
+ */
+BuildingManager.prototype.add = function (properties) {
+    let r = _.cloneDeep(properties);
+    r["id"] = this.nextBuildingID++;
+    this.buildings.push(createBuilding(r));
+
+};
+
+
+/**
+ * 
+ * @param {Object} properties.id - ID of the building 
+ */
+BuildingManager.prototype.upgrade = function (properties) {
+    let target = _.find(
+        this.buildings,
+        function (building) { return building.id === properties.id; }
+    );
+    if (target === undefined) {
+        throw new ReferenceError("Building ID not exists.");
+    }
+    else {
+        target.upgrade();
+    }
+};
+
+
+/**
+ * 
+ * @param {Object} properties.buildingID - ID of the building
+ * 
+ */
+BuildingManager.prototype.addComponent = function (properties) {
+    let target = _.find(
+        this.buildings,
+        function (building) {
+            return building.id === properties.buildingID;
+        }
+    );
+    if (target === undefined) {
+        throw new ReferenceError("Building ID not exists.");
+    }
+    else {
+        let properties_revised = _.cloneDeep(properties);
+        properties_revised["id"] = this.nextBuildingComponentID++;
+        target.addComponent(properties_revised);
+    }
+};
+
+BuildingManager.prototype.debugPrint = function () {
+    console.log("[BuildingManager DebugPrint]");
+    console.log("building number: " + this.buildings.length);
+    for (let b of this.buildings) {
+        b.debugPrint();
+    }
+    console.log("------------------------------------------------------");
+};
 
 /**
  * warpped function for new BuildingManager(...)
