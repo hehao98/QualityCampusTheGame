c81739a860a3e12a811f71464a898aaf58cd54bb
Hao He <hehao1998@pku.edu.cn>
2019-11-12 10:03:40 +0800

    feat: event system(in progress)

---
+++ assets/Scripts/GameLogic/EventManager.js
@@ -0,0 +1,41 @@
+// Class EventManager
+// For each game tick, it iteratively checks whether the condition of 
+// some events are met, and generate these events
+
+let EventSpecification = require("EventSpecification");
+
+class EventManager {
+    /**
+     * @param {BuildingManager} properties.buildingManager
+     * @param {StudentManager} properties.studentManager
+     */
+    constructor(properties) {
+        this.buildingManager = properties.buildingManager;
+        this.studentManager = properties.studentManager;
+        this.holePosts = [];
+        this.bbsTopics = [];
+        // constructor left-overs
+    }
+}
+
+EventManager.prototype.init = function() {
+    
+};
+
+EventManager.prototype.update = function() {
+
+};
+
+EventManager.prototype.getStudentOverallSatisfaction = function() {
+
+};
+
+EventManager.prototype.getStudentCanteenSatisfaction = function() {
+
+};
+
+EventManager.prototype.getStudentStudySatisfaction = function() {
+
+};
+
+module.exports = EventManager;
