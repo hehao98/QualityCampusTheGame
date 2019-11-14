d8888d20c8918b580500a18388efe618d8d3bf7c
TP <173043733@qq.com>
2019-11-14 12:52:20 +0800

    feat: add 5 new building components with debuff.

--- assets/Scripts/GameLogic/BuildingComponentSpecifications.js
+++ assets/Scripts/GameLogic/BuildingComponentSpecifications.js
@@ -2,18 +2,76 @@ let BuildingComponentSpecifications = {
     clean: {
         0: {
             defaultProperties: {
-
+                description: "",
+                picturePath: "",
+                iconPath: "",
             },
         },
         1: {
             defaultProperties: {
-
+                description: "",
+                picturePath: "",
+                iconPath: "",
             },
         },
     },
+    
     noRepair: {
+        0: {
+            defaultProperties: {
+                name: "皇帝的新修理工",
+                description: "该建筑内大量设施损坏而不见修理。",
+                capacity: -0.3,
+                picturePath: "",
+                iconPath: "",
+            }
+        }
+    },
+    buildingAtNight: {
+        0: {
+            defaultProperties: {
+                name: "夜间施工",
+                description: "该建筑附近有工地夜间施工，学生难以安眠。",
+                relaxationSatisfaction: -0.3,
+                picturePath: "",
+                iconPath: "",
+            }
+        }
+    },
+    unstableWaterTemperature: {
+        0: {
+            defaultProperties: {
+                name: "薛定谔的水温",
+                description: "浴室水温忽高忽低。小概率导致学生感冒/烧伤。",
+                cleaningSatisfaction: -0.2,
+                picturePath: "",
+                iconPath: "",
+            }
+        }
+    },
+    dirtyFood: {
+        0: {
+            defaultProperties: {
+                name: "屡教不改",
+                description: "该餐厅因食品安全问题被长期投诉，真诚道歉，永不悔改。",
+                cleaningSatisfaction: -0.2,
+                picturePath: "",
+                iconPath: "",
+            }
+        }
+    },
+    highHCHO: {
+        0: {
+            defaultProperties: {
+                name: "高效人肉除甲醛",
+                description: "该建筑内甲醛超标。",
+                cleaningSatisfaction: -0.2,
+                picturePath: "",
+                iconPath: "",
+            }
+        }
+    },
 
-    }
 };
 
 module.exports = BuildingComponentSpecifications;
