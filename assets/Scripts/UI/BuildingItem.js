d8a443a389f33b4a12c5f01b42c111ff47d11998
liaomingyang <1216007099@qq.com>
2019-11-16 15:37:26 +0800

    feat:finish the response to buildNewBuilding button and buildingList button

---
+++ assets/Scripts/UI/BuildingItem.js
@@ -0,0 +1,50 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+let Globals = require("GlobalVariables");
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        pageView: cc.PageView,
+        button: cc.Button
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+        let that = this;
+        this.pageView = Globals.UI.buildingListPageView;
+        this.button = this.getComponent(cc.Button);
+        this.button.node.on(cc.Node.EventType.TOUCH_END, function (event) {
+            that.pageView.scrollToPage(0, 1);
+        });
+    },
+
+    // update (dt) {},
+});
