dea94e6056543f017d3a10877b56fc62714618e4
liaomingyang <1216007099@qq.com>
2019-11-17 23:28:47 +0800

    feat: add building Picture

--- assets/Scripts/UI/BuildingPage.js
+++ assets/Scripts/UI/BuildingPage.js
@@ -13,6 +13,7 @@ let utilities = require("utilities");
 let BuildingItem = require("BuildingItem");
 let BuildingSpecifications = require("BuildingSpecifications");
 let BuildingIconsDict = new Array();
+let BuildingPicturesDict = new Array();
 
 cc.Class({
     extends: cc.Component,
@@ -65,6 +66,20 @@ cc.Class({
                 }
             });
         }
+
+        for (let i = 0; i < buildingTypeArr.length; ++i) {
+            let iconUrl = "Pictures/" + buildingTypeArr[i];
+            let that = this;
+            cc.loader.loadRes(iconUrl, cc.SpriteFrame, function (err, spriteFrame) {
+                BuildingPicturesDict[buildingTypeArr[i]] = spriteFrame;
+                let buildingLists = that.game.buildingManager.getBuildingLists();
+                let buildingListSize = buildingLists.length;
+                if (buildingListSize > 0) {
+                    that.showSelectedBuildingInfo(0);
+                }
+            });
+        }
+
         this.updateBuildingListInfo();
         let buildingLists = this.game.buildingManager.getBuildingLists();
         let buildingListSize = buildingLists.length;
@@ -104,10 +119,12 @@ cc.Class({
         buildingName.string = building.type;
         let buildingLevel = node.getChildByName("BuildingLevel").getComponent(cc.Label);
         buildingLevel.string = utilities.numberToRoman(building.tier + 1);
+        let buildingPicture = node.getChildByName("BuildingPhoto").getComponent(cc.Sprite);
+        buildingPicture.spriteFrame = BuildingPicturesDict[building.type];
         let buildingDescription = node.getChildByName("Description").getComponent(cc.Label);
-        buildingDescription.string = "xxxxxx";
+        buildingDescription.string = BuildingSpecifications[building.type][building.tier]["defaultProperties"]["description"];
         let buildingEffects = node.getChildByName("Effects").getComponent(cc.Label);
-        buildingEffects.string = "000000";
+        buildingEffects.string = BuildingSpecifications[building.type][building.tier]["defaultProperties"]["effects"];
     },
 
     // update (dt) {},
@@ -122,6 +139,8 @@ cc.Class({
         for (let i = 0; i < buildingTypeArr.length; ++i) {
             let buildingProperties = BuildingSpecifications[buildingTypeArr[i]][0]["defaultProperties"];
             let node = cc.instantiate(this.specificationPrefab);
+            let buildingPicture = node.getChildByName("BuildingPhoto").getComponent(cc.Sprite);
+            buildingPicture.spriteFrame = BuildingPicturesDict[buildingTypeArr[i]];
             let buildingSprite = node.getChildByName("BuildingSprite").getComponent(cc.Sprite);
             buildingSprite.spriteFrame = BuildingIconsDict[buildingTypeArr[i]];
             let buildingName = node.getChildByName("BuildingNameLabel").getComponent(cc.Label);
