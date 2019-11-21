757eb567542a99e98ddb3e12a12cfe5b27a2f7c8
Hao He <hehao1998@pku.edu.cn>
2019-11-21 10:43:43 +0800

    feat: finished resource panel

---
+++ assets/Scripts/UI/ResourceDetailPanel.js
@@ -0,0 +1,52 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: () => ({
+        game: require("Game"),
+        labels: [cc.Label]
+    }),
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start() {},
+
+    updatePanel() {
+        this.labels.forEach(label => {
+            let value = 0;
+            switch (label.node.name) {
+            case "TotalResourceChangeLabel":
+                value = this.game.fund.getWeeklyModification();
+                break;
+            case "FixedIncomeLabel":
+                value = this.game.fund.getWeeklyModification("fixed");
+                break;
+            case "BuildingIncomeLabel":
+                value = this.game.fund.getWeeklyGain("building");
+                break;
+            case "BuildingCostLabel":
+                value = this.game.fund.getWeeklyCost("building");
+                break;
+            case "StudentIncomeLabel":
+                value = this.game.fund.getWeeklyGain("student");
+                break;
+            case "TeacherCostLabel":
+                value = this.game.fund.getWeeklyCost("teacher");
+                break;
+            }
+
+            if (value > 0) {
+                label.node.color = cc.Color.GREEN;
+                label.string = "+" + value;
+            } else if (value < 0) {
+                label.node.color = cc.Color.RED;
+                label.string = value;
+            } else {
+                label.node.color = cc.Color.WHITE;
+                label.string = value;
+            }
+            label.string += "万";
+        }, this);
+    }
+});
---
+++ assets/UITest/TestResourceDetailPanel.js
@@ -0,0 +1,46 @@
+let Globals = require("GlobalVariables");
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: () => ({
+        game: require("Game"),
+        currentTick: 0
+    }),
+
+    // LIFE-CYCLE CALLBACKS:
+    
+    onLoad() {
+        Globals.TEST_MODE = true;
+    },
+
+    start() {
+        this.currentTick = this.game.currentTick;
+        this.game.speedModifier = 0.1;
+        this.game.fund.addModifier({
+            type: "student",
+            amount: 100,
+        });
+        this.game.fund.addModifier({
+            type: "teacher",
+            amount: -100,
+        });
+        this.game.fund.addModifier({
+            type: "building",
+            amount: 10,
+        });
+        this.game.fund.addModifier({
+            type: "building",
+            amount: -20,
+        });
+    },
+
+    update() {
+        if (Globals.TEST_MODE) {
+            if (this.game.currentTick === this.currentTick) {
+                return;
+            } 
+            this.currentTick = this.game.currentTick;
+        }
+    },
+});
