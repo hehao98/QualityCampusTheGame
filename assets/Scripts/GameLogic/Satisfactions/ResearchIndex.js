5f3dbc3a4c74b227b57cfc6249d0cc94fcdc0694
TP <173043733@qq.com>
2019-12-13 23:14:39 +0800

    feat: research index.

---
+++ assets/Scripts/GameLogic/Satisfactions/ResearchIndex.js
@@ -0,0 +1,36 @@
+// ResearchIndex abstruct all in-game ResearchIndex
+let utilities = require("utilities");
+let Index = require("Index");
+let Globals = require("GlobalVariables");
+
+/**
+ * 
+ * @param -  
+ */
+class ResearchIndex extends Index {
+    constructor(properties) {
+        super(properties);
+    }
+}
+
+// methods
+
+/**
+ *
+ */
+ResearchIndex.prototype.debugPrint = function () {
+    utilities.log(this.value);
+};
+
+/**
+ *
+ */
+ResearchIndex.prototype.update = function () {
+    const student = Globals.studentManager.getStudentById(this.studentID);
+    const dailyGain = Globals.universityLevelModifiers.careerTrainingProvided *
+        student.indexes.studyIndex.value / 
+        (8 * Globals.TICKS_SEMESTER);
+    this.value = this.value + dailyGain;
+};
+
+module.exports = ResearchIndex;
